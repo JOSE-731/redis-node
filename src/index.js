679ea13bf88e3296aa6ae462a43a4042243c75fe
Jose David <jose7312000@gmail.com>
2022-10-28 12:05:02 -0500

    Haciendo peticiones get con axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
+const responseTime = require('response-time');
 
+//Middleware de expres
+app.use(responseTime());
+app.get('/character', async (req, res) => {
+    const response = await axios.get('https://rickandmortyapi.com/api/character');
+    res.send(response.data)
+})
 app.listen(3000, () =>{
     console.log("Corriendo sin problemas");
 });
