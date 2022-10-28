148a82e6e271578c8ebb567712f6673cf51e5ac0
Jose David <jose7312000@gmail.com>
2022-10-28 16:49:45 -0500

    Optimizando

--- src/index.js
+++ src/index.js
@@ -3,41 +3,38 @@ const axios = require('axios');
 const app = express();
 const responseTime = require('response-time');
 const redis = require('redis');
+const { promisify } = require('util');; //Libreria por defecto en node para trabajar con promesas
 
 const client = redis.createClient({
     host: "127.0.0.1",
     port: 6379
 })
 
+//Optimizamos el callcback a una promesa para utilizar el async y el await
+const GET_ASYN = promisify(client.get).bind(client)
+const SET_ASYN = promisify(client.set).bind(client)
+
 //Middleware de expres
 app.use(responseTime());
 
 app.get('/character', async (req, res) => {
 
-    //Verificar si la data está en redis, si no está hace la peticion de los datos
-    client.get("characters", async (err, reply) => {
-        if (reply)
-            //Convertir de string a Objeto
-            return res.json(JSON.parse(reply))
+    //Verificar si la data está en redis, si no está hace la peticion de los datos (Responde desde la memoria cache)
+    const reply = await GET_ASYN('characters');
+    if (reply)
+        //Convertir de string a Objeto
+        return res.json(JSON.parse(reply))
 
         //Peticion a una api
         const response = await axios.get('https://rickandmortyapi.com/api/character');
 
         //Redis guarda en string, por lo que debemos pasar el objeto a string
-        const data = JSON.stringify(response.data);
-
-        //Guardamos en redis
-        client.set('characters', data, (err, reply) => {
-
-            if (err) console.log(err)
-
-            console.log(reply)
-            res.json(response.data)
-        });
-    });
-
+        const reply2 = await SET_ASYN('characters', JSON.stringify(response.data));
+        res.json(reply2.data)
 
 });
+
+
 app.listen(3000, () => {
     console.log("Corriendo sin problemas");
 });
